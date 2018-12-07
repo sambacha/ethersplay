f0e38002f89cd0b1e1d007bcfa8316af5e661902
Michael Rodler <michael.rodler@paluno.uni-due.de>
2018-12-07 15:07:50 +0100

    Added lookup of function hashes from 4byte.directory
    UI:
    * Context menu entry to rename all functions (works globally)
    * Context menu entry to lookup/rename one 4byte hash (works on PUSH instructions)
    
    Notes:
    * Requests are cached in a local file
    * 4byte signature collisions are reported as comments

--- ethersplay/__init__.py
+++ ethersplay/__init__.py
@@ -5,6 +5,7 @@ from coverage import function_coverage_start
 from print_stack import function_printStack_start
 from stack_value_analysis import function_stack_value_analysis_start
 from evm import EVM, EVMView
+import lookup4byte
 
 
 def is_valid_evm(view, function=None):
@@ -30,6 +31,18 @@ PluginCommand.register_for_function("EVM Print stack",
                                     "Print up to 10 values of the stack",
                                     function_printStack_start,
                                     is_valid=is_valid_evm)
+
+PluginCommand.register(
+        "EVM Rename functions (4byte.directory)",
+        "Perform lookup of all hash signatures on 4byte.directory to rename unknown functions",
+        lookup4byte.rename_all_functions,
+        is_valid=is_valid_evm)
+
+PluginCommand.register_for_address(
+        "EVM Lookup 4byte hash (4byte.directory)",
+        "Perform lookup of one hash signature on 4byte.directory",
+        lookup4byte.lookup_one_inst,
+        is_valid=is_valid_evm)
 
 
 EVM.register()
---
+++ ethersplay/lookup4byte.py
@@ -0,0 +1,190 @@
+import os
+import json
+import atexit
+
+import binaryninja as bn
+from binaryninja import log_error, log_info
+
+log_debug = log_info
+
+try:
+    import requests
+except ImportError:
+    requests = None
+
+LOOKUP_4BYTE_URL = "https://www.4byte.directory/api/v1/signatures/"
+CACHE_4BYTE_PATH = os.path.expanduser("~/.4byte_cache")
+CACHE_4BYTE_FILE = os.path.join(CACHE_4BYTE_PATH, "cache.json")
+
+_4byte_cache = None
+
+
+def load_4byte_cache():
+    global _4byte_cache
+    log_debug("Loading 4byte lookup cache from: " + str(CACHE_4BYTE_PATH))
+    if os.path.exists(CACHE_4BYTE_FILE):
+        with open(CACHE_4BYTE_FILE) as f:
+            _4byte_cache = json.load(f)
+            log_debug("4byte cache load success!")
+    else:
+        _4byte_cache = {}
+    log_debug("Cache contains {} entries".format(len(_4byte_cache)))
+
+
+def save_4byte_cache():
+    if _4byte_cache:
+        if not os.path.exists(CACHE_4BYTE_PATH):
+            os.makedirs(CACHE_4BYTE_PATH)
+        with open(CACHE_4BYTE_FILE, "w") as f:
+            json.dump(_4byte_cache, f)
+
+
+def init_cache():
+    if _4byte_cache is None:
+        load_4byte_cache()
+        log_debug("atexit handler: Saving 4byte lookup cache in " + str(CACHE_4BYTE_PATH))
+        atexit.register(save_4byte_cache)
+
+
+def lookup_hash(sig):
+    global _4byte_cache
+    assert _4byte_cache is not None
+
+    if sig in _4byte_cache and _4byte_cache[sig]:
+        return _4byte_cache[sig]
+
+    if not requests:
+        log_error("couldn't import requests for fetching from 4byte.directory")
+        return []
+    try:
+        res = requests.get(LOOKUP_4BYTE_URL, params={"hex_signature": sig})
+        rj = res.json()
+        results = rj['results']
+
+        if len(results) >= 1:
+            sig_collisions = [r['text_signature'] for r in results]
+            _4byte_cache[sig] = sig_collisions
+            return sig_collisions
+        else:
+            log_debug("4.byte directory didn't yield any results")
+            return []
+    except AssertionError:
+        raise
+    except Exception as e:
+        log_error("4byte lookup failed, reason ({}): {}"
+                  .format(type(e), e))
+        return []
+
+    return []
+
+
+def reset_symbol(bv, imm, hash_value, method_name):
+    # we don't create symbols inside of actual address space. This would
+    # interfere with actual functions... e.g., the hash 0x00000000 would result
+    # in the dispatcher being renamed.
+    if imm < len(bv):
+        return
+
+    sym = bv.get_symbol_at(imm)
+    if sym is not None:
+        bv.undefine_user_symbol(sym)
+    bv.define_user_symbol(
+        bn.Symbol(
+            bn.SymbolType.ImportedFunctionSymbol,
+            imm,
+            '{} -> {}'.format(
+                hash_value, method_name
+            )
+        )
+    )
+
+
+def format_comment(sigs):
+    assert len(sigs) >= 1
+    text_sig = sigs[0]
+    comment = ""
+    if len(sigs) > 1:
+        comment = ("signatures with colliding hash:\n" + "\n".join(sigs))
+    return text_sig, comment
+
+
+def rename_all_functions(bv):
+    init_cache()
+
+    for function in bv.functions:
+        if function.name.startswith("#"):
+            log_info("performing 4byte lookup for '{}'".format(function.name))
+            try:
+                sig = "0x" + function.name[1:].strip()
+                sigs = lookup_hash(sig)
+                if len(sigs) >= 1:
+                    new_name, comment = format_comment(sigs)
+                    function.name = new_name
+
+                    imm = int(sig, 16)
+                    hash_value = "#{:0=8x}".format(imm)
+                    reset_symbol(bv, imm, hash_value, new_name)
+                    if function.comment:
+                        function.comment += "\n------\n"
+                    function.comment += comment
+                log_info("found {} text sigs for hash {} renamed function to {}"
+                         .format(len(sigs), sig, function.name))
+            except AssertionError:
+                raise
+            except Exception as e:
+                log_error("4byte lookup failed for function '{}' reason ({}): {}"
+                          .format(function.name, type(e), e))
+
+    save_4byte_cache()
+    return 0
+
+
+def lookup_one_inst(bv, address):
+    init_cache()
+
+    disas = bv.get_disassembly(address).strip()
+    try:
+        inst = disas.split(" ")[0]
+        if not inst.startswith("PUSH"):
+            log_error("Instruction '{}' at address {} is not a PUSH inst"
+                      .format(inst, address))
+            return -1
+        if "#" not in disas:
+            log_error("invalid PUSH immediate value")
+            return -1
+
+        imm = int("0x" + disas.strip().split("#")[-1], 16)
+        log_info("EVM: 4byte lookup of hash: {}".format(imm))
+
+        # we mask the top bytes
+        imm = imm & 0xffffffff
+
+        sig = "0x{:0=8x}".format(imm)
+        hash_value = "#{:0=8x}".format(imm)
+
+        sigs = lookup_hash(sig)
+        log_debug("found {} sigs: {}".format(len(sigs), sigs))
+        if len(sigs) == 0:
+            return 0
+
+        method_name, comment = format_comment(sigs)
+
+        reset_symbol(bv, imm, hash_value, method_name)
+
+        if comment:
+            for func in bv.get_functions_containing(address):
+                log_debug("in function {}".format(func))
+                c = func.get_comment_at(address)
+                if "sign" not in c:
+                    log_debug("setting comment")
+                    c = "{}\n---\n{}".format(c, comment)
+                    func.set_comment_at(address, c)
+
+    except AssertionError:
+        raise
+    except Exception as e:
+        log_error("4byte lookup failed for inst {} at address '{}' reason ({}): {}"
+                  .format(disas, address, type(e), e))
+
+    save_4byte_cache()
+    return 0
